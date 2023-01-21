6891545641afa668c771afeac741a542210767ad
Gea Loro <gloro@ucsc.edu>
2023-01-20 18:16:51 -0800

    Update asgn1/memory.c

--- asgn1/memory.c
+++ asgn1/memory.c
@@ -1,3 +1,14 @@
 #include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main() {
+
+    fd = open("file", 0_RDONLY);
+    if (fd == -1)
+    {
+        printf("Operation Failed");
+    }
 
 
+}
